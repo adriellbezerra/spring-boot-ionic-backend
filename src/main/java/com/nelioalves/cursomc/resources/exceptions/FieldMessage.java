147b6ab8e65fbd34042c4e03ae462e97d7a71653
Adriell Bezerra <adrielprof01@gmail.com>
2020-05-06 14:34:00 -0300

    Validação Customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/FieldMessage.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/FieldMessage.java
@@ -6,7 +6,7 @@ public class FieldMessage implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
-	private String fielName;
+	private String fieldName;
 	private String message;
 
 	public FieldMessage() {
@@ -15,16 +15,16 @@ public class FieldMessage implements Serializable{
 
 	public FieldMessage(String fielName, String message) {
 		super();
-		this.fielName = fielName;
+		this.fieldName = fielName;
 		this.message = message;
 	}
 
-	public String getFielName() {
-		return fielName;
+	public String getFieldName() {
+		return fieldName;
 	}
 
-	public void setFielName(String fielName) {
-		this.fielName = fielName;
+	public void setFieldName(String fielName) {
+		this.fieldName = fielName;
 	}
 
 	public String getMessage() {
